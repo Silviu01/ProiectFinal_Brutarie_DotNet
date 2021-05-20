759c2c268f9d17f0a9791deb47ad5a9fe18d7f52
Claudia C <claudia.ceclan92@yahoo.com>
2021-05-20 20:11:39 +0300

    Updates2

--- Brutarie_Proiect_Final/Brutarie_Proiect_Final/InventarBrutarie.cs
+++ Brutarie_Proiect_Final/Brutarie_Proiect_Final/InventarBrutarie.cs
@@ -22,7 +22,36 @@ namespace Brutarie_Proiect_Final
 
                     if (raspuns == 1)
                     {
-                        Panificatie.ProcesarePanificatie();
+                        Console.Clear();
+                        Meniu.AfiseazaMeniuAdaugaProduseInStoc();
+
+                        bool q2 = true;
+
+                        while (q2)
+                        {
+                            int raspuns2 = int.Parse(Console.ReadLine());
+                            if (raspuns2 == 0)
+                            {
+                                Meniu.AfiseazaMeniuPrincipal();
+                            }
+                            else if (raspuns2 == 1)
+                            {
+                                Panificatie.ProcesarePanificatie();
+                            }
+                            else if (raspuns2 == 2)
+                            {
+                                Console.WriteLine("Pateu Merdenea Covrig Corn");
+                            }
+                            else if (raspuns2 == 3)
+                            {
+                                Console.WriteLine("Ecler Amandina Savarina Tort");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Optiunea nu corespunde cu nici un obiect din lista. Alege din nou:");
+                            }
+
+                        }
 
                     }      
                 }
