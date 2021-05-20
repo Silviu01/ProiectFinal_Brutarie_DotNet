c060508a01958fc66062a38e808c718a1e95846c
Claudia C <claudia.ceclan92@yahoo.com>
2021-05-20 18:59:20 +0300

    Class Creation

---
+++ Brutarie_Proiect_Final/Brutarie_Proiect_Final/Meniu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brutarie_Proiect_Final
+{
+    class Meniu
+    {
+        public static void AfiseazaMeniuPrincipal()
+        {
+            Console.WriteLine("Alege numarul corespunzator uneia dintre optiunile de mai jos:");
+            Console.WriteLine("\n1.Adaugă produse în stoc");
+            Console.WriteLine("2.Vinde produse");
+            Console.WriteLine("3.Elimină produse din stoc");
+            Console.WriteLine("4.Înregistrează comenzi");
+            Console.WriteLine("5.Afisează stoc si vânzări");
+            Console.WriteLine("0.Închidere program");
+        }
+
+
+        public static void AfiseazaMeniuAdaugaProduseInStoc()
+        {
+            Console.WriteLine("Doriti sa adaugati:");
+            Console.WriteLine("1.Panificatie");
+            Console.WriteLine("2.Patiserie");
+            Console.WriteLine("3.cofetarie");
+            Console.WriteLine("0.Revenire la ecranul anterior");
+
+        }
+
+    }
+}
---
+++ Brutarie_Proiect_Final/Brutarie_Proiect_Final/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Brutarie_Proiect_Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool q = true;
+
+            while (q)
+            {
+                Meniu.AfiseazaMeniuPrincipal();
+                int raspuns = int.Parse(Console.ReadLine());
+
+                if (raspuns == 1 || raspuns == 2 || raspuns == 3 || raspuns == 4 || raspuns == 5)
+                {
+
+                    if (raspuns == 1)
+                    {
+                        Console.Clear();
+                        Meniu.AfiseazaMeniuAdaugaProduseInStoc();
+
+                        int raspuns2 = int.Parse(Console.ReadLine());
+                        if (raspuns2 == 0)
+                        {
+                            Meniu.AfiseazaMeniuPrincipal();
+                        }
+                    }
+
+                }
+                else if (raspuns == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Optiunea nu corespunde cu nici un obiect din lista. Alege din nou:");
+                }
+            }
+
+
+
+
+
+
+        }
+    }
+}
